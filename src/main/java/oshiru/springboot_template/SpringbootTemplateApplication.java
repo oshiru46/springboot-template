409a8c077b54d827f896fa2bd81281d3f0d3d06d
Oshiru <oshiru46@gmail.com>
2024-09-13 23:38:01 +0900

    initial commit

---
+++ src/main/java/oshiru/springboot_template/SpringbootTemplateApplication.java
@@ -0,0 +1,13 @@
+package oshiru.springboot_template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oshiru/springboot_template/SpringbootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package oshiru.springboot_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
